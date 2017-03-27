da9d19265cf6f3d4b37148f2afd92a91898afcd9
tomeradmon <tomer.a@quali.com>
2017-03-27 15:34:36 +0300

    some change

--- db_create.py
+++ db_create.py
@@ -8,6 +8,7 @@ db.session.commit()
 
 admin = User('tomer', 'admon', is_admin=True)
 guest = User(u'תומר', u'אדמון')
+guest = User(u'תומר', u'נחשון')
 
 avoda = Party(u'העבודה', 'https://www.am-1.org.il/wp-content/uploads/2015/03/%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94.-%D7%A6%D7%99%D7%9C%D7%95%D7%9D-%D7%99%D7%97%D7%A6.jpg')
 likud = Party(u'הליכוד', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Likud_Logo.svg/250px-Likud_Logo.svg.png')
